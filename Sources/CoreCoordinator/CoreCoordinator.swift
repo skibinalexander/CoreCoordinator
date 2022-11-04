8a22725acc43c05a2a9e78d014f3ec9779bb6a6c
skibinalexander <skibinalexander@gmail.com>
2022-11-04 22:12:10 +0300

    Initial Commit

---
+++ Sources/CoreCoordinator/CoreCoordinator.swift
@@ -0,0 +1,6 @@
+public struct CoreCoordinator {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CoreCoordinatorTests/CoreCoordinatorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CoreCoordinator
+
+final class CoreCoordinatorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CoreCoordinator().text, "Hello, World!")
+    }
+}
